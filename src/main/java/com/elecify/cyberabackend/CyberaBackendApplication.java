62404dd931e52df794ec34f57feea598a7189338
nulldeath <krawdev@gmail.com>
2023-08-01 22:28:29 -0300

    init project

---
+++ src/main/java/com/elecify/cyberabackend/CyberaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.elecify.cyberabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberaBackendApplication.class, args);
+    }
+
+}
